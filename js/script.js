a9ea09ab4e9d28efed2072e31c20fd384dba6ed7
Antonio Barajas <antonio.barajas@ge.com>
2017-04-21 23:05:47 -0500

    Updpate Add Settings information on HTML

--- js/script.js
+++ js/script.js
@@ -1,128 +1,122 @@
-var chokidar = require('chokidar');
-var fs = require('fs');
-var watcher = null;
-var showInLogFlag = false;
+var scriptApp = angular.module('scriptApp',[]);
 
-function StartWatcher(path){
-    document.getElementById("start").disabled = true;
-    document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
+scriptApp.controller('scriptController',function($scope,$http){
 
-    watcher = chokidar.watch(path, {
-        ignored: /[\/\\]\./,
-        persistent: true
+    $http.get("./js/fileSettings.json")
+    .then(function(res){
+        $scope.init(res.data[0]);
     });
 
-    function onWatcherReady(){
-        console.info('From here can you check for real changes, the initial scan has been completed.');
-        showInLogFlag = true;
-        document.getElementById("stop").style.display = "block";
-        document.getElementById("messageLogger").innerHTML = "The path is now being watched";
+    $scope.textExtensions="";
+    var chokidar = require('chokidar');
+    var fs = require('fs');
+    var watcher = null;
+    var showInLogFlag = false;
+
+    $scope.init = function(fileSettings) {
+        $scope.fileSettings = fileSettings;
+    };
+
+    function StartWatcher(path){
+        document.getElementById("start").disabled = true;
+        document.getElementById("messageLogger").innerHTML = "Scanning the path, please wait ...";
+
+        watcher = chokidar.watch(path, {
+            ignored: /[\/\\]\./,
+            persistent: true
+        });
+
+        function onWatcherReady(){
+            console.info('From here can you check for real changes, the initial scan has been completed.');
+            showInLogFlag = true;
+            document.getElementById("stop").style.display = "block";
+            document.getElementById("messageLogger").innerHTML = "The path is now being watched";
+        }
+
+        watcher
+        .on('add', function(path) {
+            console.log('File', path, 'has been added');
+
+            if(showInLogFlag){
+                addLog("File added : "+path,"new");
+            }
+        })
+        .on('addDir', function(path) {
+            console.log('Directory', path, 'has been added');
+
+            if(showInLogFlag){
+                addLog("Folder added : "+path,"new");
+            }
+        })
+        .on('change', function(path) {
+            console.log('File', path, 'has been changed');
+
+            if(showInLogFlag){
+                addLog("A change ocurred : "+path,"change");
+            }
+        })
+        .on('unlink', function(path) {
+            console.log('File', path, 'has been removed');
+
+            if(showInLogFlag){
+                addLog("A file was deleted : "+path,"delete");
+            }
+        })
+        .on('unlinkDir', function(path) {
+            console.log('Directory', path, 'has been removed');
+
+            if(showInLogFlag){
+                addLog("A folder was deleted : "+path,"delete");
+            }
+        })
+        .on('error', function(error) {
+            console.log('Error happened', error);
+
+            if(showInLogFlag){
+                addLog("An error ocurred: ","delete");
+                console.log(error);
+            }
+        })
+        .on('ready', onWatcherReady)
+        .on('raw', function(event, path, details) {
+            // This event should be triggered everytime something happens.
+            console.log('Raw event info:', event, path, details);
+        });
     }
 
-    watcher
-    .on('add', function(path) {
-        console.log('File', path, 'has been added');
+    getFolderDir = function(idText) {
+        const {dialog} = require('electron').remote;
+        dialog.showOpenDialog({
+            properties: ['openDirectory']
+        },function(path){
+            if(path){
+                $("#"+idText).value=path[0];
+            }else {
+                console.log("No path selected");
+            }
+        });
+    };
 
-        if(showInLogFlag){
-            addLog("File added : "+path,"new");
+    function resetLog(){
+        return document.getElementById("log-container").innerHTML = "";
+    }
+
+    function addLog(message,type){
+        var el = document.getElementById("log-container");
+        var newItem = document.createElement("LI");       // Create a <li> node
+        var textnode = document.createTextNode(message);  // Create a text node
+        if(type == "delete"){
+            newItem.style.color = "red";
+        }else if(type == "change"){
+            newItem.style.color = "blue";
+        }else{
+            newItem.style.color = "green";
         }
-    })
-    .on('addDir', function(path) {
-         console.log('Directory', path, 'has been added');
 
-         if(showInLogFlag){
-             addLog("Folder added : "+path,"new");
-         }
-     })
-    .on('change', function(path) {
-        console.log('File', path, 'has been changed');
+        newItem.appendChild(textnode);                    // Append the text to <li>
+        el.appendChild(newItem);
+    }
 
-        if(showInLogFlag){
-            addLog("A change ocurred : "+path,"change");
-        }
-    })
-    .on('unlink', function(path) {
-        console.log('File', path, 'has been removed');
+    $scope.init();
 
-        if(showInLogFlag){
-            addLog("A file was deleted : "+path,"delete");
-        }
-    })
-    .on('unlinkDir', function(path) {
-        console.log('Directory', path, 'has been removed');
-
-        if(showInLogFlag){
-            addLog("A folder was deleted : "+path,"delete");
-        }
-    })
-    .on('error', function(error) {
-        console.log('Error happened', error);
-
-        if(showInLogFlag){
-            addLog("An error ocurred: ","delete");
-            console.log(error);
-        }
-    })
-    .on('ready', onWatcherReady)
-    .on('raw', function(event, path, details) {
-        // This event should be triggered everytime something happens.
-        console.log('Raw event info:', event, path, details);
-    });
-}
-
-document.getElementById("startFolderButton").addEventListener("click",function(e){
-    const {dialog} = require('electron').remote;
-    dialog.showOpenDialog({
-        properties: ['openDirectory']
-    },function(path){
-        if(path){
-            document.getElementById("startFolder").innerHTML=path[0];
-        }else {
-            console.log("No path selected");
-        }
-    });
-},false);
-
-document.getElementById("endFolderButton").addEventListener("click",function(e){
-    const {dialog} = require('electron').remote;
-    dialog.showOpenDialog({
-        properties: ['openDirectory']
-    },function(path){
-        if(path){
-            document.getElementById("endFolder").innerHTML=path[0];
-        }else {
-            console.log("No path selected");
-        }
-    });
-},false);
-
- document.getElementById("stop").addEventListener("click",function(e){
-     if(!watcher){
-         console.log("You need to start first the watcher");
-     }else{
-         watcher.close();
-         document.getElementById("start").disabled = false;
-         showInLogFlag = false;
-         document.getElementById("messageLogger").innerHTML = "Nothing is being watched";
-     }
- },false);
-
- function resetLog(){
-     return document.getElementById("log-container").innerHTML = "";
- }
-
- function addLog(message,type){
-     var el = document.getElementById("log-container");
-     var newItem = document.createElement("LI");       // Create a <li> node
-     var textnode = document.createTextNode(message);  // Create a text node
-     if(type == "delete"){
-         newItem.style.color = "red";
-     }else if(type == "change"){
-         newItem.style.color = "blue";
-     }else{
-         newItem.style.color = "green";
-     }
-
-     newItem.appendChild(textnode);                    // Append the text to <li>
-     el.appendChild(newItem);
- }
+});
